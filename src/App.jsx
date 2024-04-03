03798b01a68adeeb5be62011b50252e050bfd78e
Emilia Iwu <emiliatech24@gmail.com>
2024-04-03 13:06:00 +0100

    chore: add lazy routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,44 @@ import {
 	RouterProvider,
 } from "react-router-dom";
 import Root from "./Root";
-import { LandingPage, NotFound } from "./landing/pages";
+import { LandingPage } from "./landing/pages";
 import {
 	SignUpPage,
 	SignInPage,
 	ForgotPasswordPage,
 	ResetPasswordPage,
 } from "./pages";
-import { AppLayout, ProjectLayout } from "./app/layout";
+import { AppLayout } from "./app/layout";
 import {
-	Dashboard,
-	Theme,
-	Task,
 	TaskBoard,
 	Details,
 	Commit,
-	TaskPage,
-	ProjectPage,
-	Library,
 	GeneralCollection,
 	CollectionPage,
-	Profile,
 	Portfolio,
 } from "./app/pages";
 import "./style/devvieLoader.css";
 import "./style/app.css";
 import { UserProfileContext } from "./app/context";
-import { useContext } from "react";
-// Lazy
-// const LazyDashboard = React.lazy
+import React, { Suspense, useContext } from "react";
+import { DevvieLoader } from "./components";
+
+// Lazy load components
+const LazyDashboard = React.lazy(() =>
+	import("./app/features/dashboard/Dashboard")
+);
+const LazyTheme = React.lazy(() => import("./app/features/theme/Theme"));
+const LazyTaskPage = React.lazy(() => import("./app/features/task/TaskPage"));
+const LazyLibrary = React.lazy(() => import("./app/features/library/Library"));
+const LazyProfile = React.lazy(() => import("./app/features/Profile/Profile"));
+const LazyProjectLayout = React.lazy(() =>
+	import("./app/layout/ProjectLayout")
+);
+const LazyProjectPage = React.lazy(() =>
+	import("./app/features/project/ProjectPage")
+);
+const LazyTask = React.lazy(() => import("./app/features/task/Task"));
+const LazyNotFound = React.lazy(() => import("./landing/pages/NotFound"));
 
 const App = () => {
 	const { userProfile } = useContext(UserProfileContext);
@@ -51,32 +60,36 @@ const App = () => {
 				<Route path='/reset-password' element={<ResetPasswordPage />} />
 
 				<Route path='user' element={<AppLayout />}>
-					<Route index element={<Dashboard />} />
-					<Route path='dashboard' element={<Dashboard />} />
-					<Route path='appearance' element={<Theme />} />
-					<Route path='tasks' element={<TaskPage />} />
-					<Route path='devmark' element={<Library />}>
+					<Route index element={<LazyDashboard />} />
+					<Route path='dashboard' element={<LazyDashboard />} />
+					<Route path='appearance' element={<LazyTheme />} />
+					<Route path='tasks' element={<LazyTaskPage />} />
+					<Route path='devmark' element={<LazyLibrary />}>
 						<Route index element={<GeneralCollection />} />
 						<Route path=':slug' element={<CollectionPage />} />
 					</Route>
-					<Route path='profile' element={<Profile />} />
-					<Route path='projects' element={<ProjectLayout />}>
-						<Route index element={<ProjectPage />} />
-						<Route path=':slug' element={<Task />}>
+					<Route path='profile' element={<LazyProfile />} />
+					<Route path='projects' element={<LazyProjectLayout />}>
+						<Route index element={<LazyProjectPage />} />
+						<Route path=':slug' element={<LazyTask />}>
 							<Route index element={<TaskBoard />} />
 							<Route path='details' element={<Details />} />
 							<Route path='commits' element={<Commit />} />
 						</Route>
-						<Route path='*' element={<NotFound />} />
+						<Route path='*' element={<LazyNotFound />} />
 					</Route>
-					<Route path='*' element={<NotFound />} />
+					<Route path='*' element={<LazyNotFound />} />
 				</Route>
-				<Route path='*' element={<NotFound />} />
+				<Route path='*' element={<LazyNotFound />} />
 			</Route>
 		)
 	);
 
-	return <RouterProvider router={router} />;
+	return (
+		<Suspense fallback={<DevvieLoader />}>
+			<RouterProvider router={router} />
+		</Suspense>
+	);
 };
 
 export default App;
